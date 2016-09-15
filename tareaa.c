1fcb40514d860f42d95c6cfe5b2c8e9ee0a4ca8a
Rangoxl <oscaar.pino@gmail.com>
2016-09-15 16:07:35 -0300

    esta es la tarea

---
+++ tareaa.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+	
+printf("hola oscar ignacio pino poblete ");
+printf ("\ncomo estas");
+
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+}
+
+//diseñe un programa que lea su nombre y luego lo salude//
